3262887427ffabfc03dcc99183bdadf0f9101025
Alex Martin <atonymartin20@yahoo.com>
2019-08-04 21:45:56 -0500

    Added 3 more event listeners

--- js/index.js
+++ js/index.js
@@ -15,8 +15,25 @@ yellowBus.addEventListener('mouseleave', function() {
     yellowBus.setAttribute('src', 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Flawtones.seattleschools.org%2FUserFiles%2FServers%2FServer_12451%2FImage%2FAbout%2520Us%2FTransportation%2FYellowBus.png&f=1');
 });
 
-// * [ ] `mouseover`
-// * [ ] `keydown`
+// Edit 'Welcome to Fun Bus!'
+const welcome = document.querySelector('.intro h2');
+document.addEventListener('keypress', function(event) {
+    if(event.key == "a") {
+        welcome.innerText = 'Please Enter Faulty WARNING .... GIRN'
+    }
+});
+
+// On key down remove bus image
+document.addEventListener('keydown', function() {
+    yellowBus.style.display = 'none';
+});
+
+const mapImg = document.querySelector('.img-content');
+mapImg.addEventListener('mouseover', function() {
+    mapImg.style.width = '25%';
+});
+
+
 // * [ ] `wheel`
 // * [ ] `drag / drop`
 // * [ ] `load`
